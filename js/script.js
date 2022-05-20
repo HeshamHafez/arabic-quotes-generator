9172cfb792c277cc331b8e78f496e4bef18a780e
HeshamHafez <heshamhafez94@gmail.com>
2022-05-20 15:52:39 +0200

    modified js file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 
+var quoteList = [
+    {
+        quote: "في قلبي صدى ما بحبش حدى.",
+        quoteCountry: "فلسطين"
+    },
+    {
+        quote: "سيدي مليح زاده الهوى والريح.",
+        quoteCountry: "الجزائر"
+    },
+    {
+        quote: "اللي باعك بالفول بيعه بالقشور.",
+        quoteCountry: "الجزائر"
+    },
+    {
+        quote: "إمشي في جنازة ولا تمشي في جوازة.",
+        quoteCountry: "أم الدنيا"
+    },
+    {
+        quote: "أعمِلْ كيف جارك وإلا حول باب دارك.",
+        quoteCountry: "تونس"
+    },
+    {
+        quote: "إن كنتم نسيتوا اللي جرى، هاتوا الدفاتر تنقرا.",
+        quoteCountry: "أم الدنيا"
+    }
+]
 
 function displayQuote(){
     var randomQuote = Number.parseInt(Math.random()*quoteList.length + 1);
     document.getElementById("quoteP").innerHTML = `\"${quoteList[randomQuote].quote}\"`;
+    document.getElementById("quotePlace").innerHTML = `${quoteList[randomQuote].quoteCountry}`;
 }
 
 
